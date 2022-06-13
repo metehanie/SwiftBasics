9bd80f9f9f0d1222f45b578e8134adae170467ad
Metehan Bolat <metehan.bolat.ie@gmail.com>
2022-06-14 00:56:00 +0300

    Arrays

--- Arrays.playground/Contents.swift
+++ Arrays.playground/Contents.swift
@@ -1,3 +1,17 @@
 import UIKit
 
-var greeting = "Hello, playground"
+// Listeyi Any değerinde yaparsak içerisine her değeri alabiliriz.
+var myFavoriteMovies: [Any] = ["Pulp Fiction", "Kill Bill", "Interstellar", 5]
+myFavoriteMovies[0]
+
+// as -> casting
+
+var myStringArray = ["Metehan" , "Bolat", "23"]
+myStringArray[0].uppercased()
+myStringArray.count
+myStringArray.last
+myStringArray.first
+// Alfabetik dizer
+myStringArray.sort()
+
+var myNumberArray = [1,2,3,4,5,6]
